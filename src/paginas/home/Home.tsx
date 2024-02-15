bb74cdf4688bc9beb8a77b49e45e0961a308314f
nataliabpo <nataliaoliveir0108@gmail.com>
2024-02-15 08:49:14 -0300

    atividade react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import'./Home.css'
+
+function Home() {
+    return (
+        <>
+            <h1 className="titulo"> É quando o vento sacode a cabeleira </h1>
+            <img className="img"src="https://jpimg.com.br/uploads/2017/04/388693593-arrastao-do-frevo-marca-encerramento-do-carnaval-no-recife.jpg"></img>
+
+    </>)
+
+}
+
+export default Home;
